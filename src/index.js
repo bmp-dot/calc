779ce79f0d0d73e5e25c6f35bf2c6bbb87358e53
bmp-dot <bmpeterson@gmail.com>
2025-03-30 02:13:40 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CalculatorApp from './App'; // Make sure App.jsx is in the same folder
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
